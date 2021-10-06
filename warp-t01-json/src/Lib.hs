95c796fa278662f4352a98ebb9176f03aba80332
mingyuchoo <mingyuchoo@gmail.com>
2021-10-06 16:17:58 +0900

    t570

--- warp-t01-json/src/Lib.hs
+++ warp-t01-json/src/Lib.hs
@@ -4,13 +4,17 @@ module Lib
     where
 
 import           Data.Aeson
-import           Data.Text                 (Text)
+import           Data.ByteString           (ByteString, length)
+import           Data.ByteString.Lazy      (fromStrict)
+import           Data.Text                 (Text, pack)
 import           Network.HTTP.Types        (status200)
 import           Network.HTTP.Types.Header (hContentType)
+import           Network.HTTP.Types.URI    (Query)
 import           Network.Wai
     ( Application
     , Response
     , pathInfo
+    , queryString
     , responseLBS
     )
 import           Network.Wai.Handler.Warp  (run)
@@ -24,20 +28,28 @@ someFunc = do
 
 -- | Application
 app :: Application
-app req f = f $
-    case pathInfo req of
-        -- Place custom routes here
-        _   -> anyRoute
+app req res = do
+    case (pathInfo req, queryString req) of
+      (["expr"], [("q", Just stuff)]) -> res $ homeRoute stuff
+      _                               -> res $ notFoundRoute
 
 -- | JSON Response
-anyRoute :: Response
-anyRoute =
+homeRoute :: ByteString -> Response
+homeRoute bs =
     responseLBS
         status200
         [(hContentType, "application/json")]
-        (encode jsonData)
+        (fromStrict bs)
 
-
+-- | Page not found
+notFoundRoute :: Response
+notFoundRoute =
+    responseLBS
+        status200
+        [(hContentType, "text/plain")]
+        "Page not found."
 
 -- | JSON data
-jsonData = ["a", "b", "c"] :: [Text]
+jsonData :: [Text]
+jsonData = (pack . show) <$> [1 .. 100]
+
