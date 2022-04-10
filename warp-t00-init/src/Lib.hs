8b15b932da364d1b826ab7cc39c7f1ae88c4622b
mingyuchoo <mingyuchoo@gmail.com>
2022-04-10 16:13:52 +0900

    updated

--- warp-t00-init/src/Lib.hs
+++ warp-t00-init/src/Lib.hs
@@ -2,13 +2,20 @@
 {-# LANGUAGE StandaloneKindSignatures #-}
 
 module Lib
-    ( someFunc
-    ) where
+    where
 
 import           Data.Kind                 ()
-import           Network.HTTP.Types        (status200)
+import           Flow                      ((<|))
+import           Network.HTTP.Types        (status200, status404)
 import           Network.HTTP.Types.Header (hContentType)
-import           Network.Wai               (Response, responseLBS)
+import           Network.Wai
+    ( Request
+    , Response
+    , ResponseReceived
+    , rawPathInfo
+    , responseFile
+    , responseLBS
+    )
 import           Network.Wai.Handler.Warp  (run)
 
 -- |
@@ -16,12 +23,62 @@ import           Network.Wai.Handler.Warp  (run)
 --
 someFunc :: IO ()
 someFunc = do
-    let port = 3000
-    putStrLn $ "listening on " ++ show port
-    run port app
+  putStrLn $ "listening on " ++ show port
+  run port app3
+  where
+    port :: Int
+    port = 3000
 
-app :: p -> (Response -> t) -> t
-app req f = f $ responseLBS
-                    status200
-                    [(hContentType, "text/plain")]
-                    "Hello, World!\n"
+
+-- |
+--
+--
+app1 :: Request                           -- ^ request
+     -> (Response -> IO ResponseReceived) -- ^ handler response to IO
+     -> IO ResponseReceived               -- ^ response
+app1 _request respond = do
+  putStrLn "app1> Received an HTTP request!"
+  respond <| responseLBS status200 [(hContentType, "text/plain")] "Hello, World!\n"
+
+
+-- |
+--
+--
+app2 :: Request                           -- ^ request
+     -> (Response -> IO ResponseReceived) -- ^ handler response to IO
+     -> IO ResponseReceived               -- ^ response
+app2 _request respond = do
+  putStrLn "app2> Received an HTTP request!"
+  respond <| responseFile status200 [(hContentType, "text/html")] "index.html" Nothing
+
+
+-- |
+--
+--
+app3 :: Request                           -- ^ request
+     -> (Response -> IO ResponseReceived) -- ^ handler response to IO
+     -> IO ResponseReceived               -- ^ response
+app3 request respond = do
+  putStrLn "app3> Received an HTTP request!"
+  respond <| case rawPathInfo request of
+    "/"     -> index
+    "/raw/" -> plainIndex
+    _       -> notFound
+
+-- |
+--
+--
+index :: Response
+index = responseFile status200 [(hContentType, "text/html")] "index.html" Nothing
+
+-- |
+--
+--
+plainIndex :: Response
+plainIndex = responseFile status200 [(hContentType, "text/plain")] "index.html" Nothing
+
+-- |
+--
+--
+notFound :: Response
+notFound = responseLBS status404 [(hContentType, "text/plain")] "404 - Not Found"
--- warp-t01-json/src/Lib.hs
+++ warp-t01-json/src/Lib.hs
@@ -4,64 +4,102 @@
 module Lib
     where
 
-import           Data.Aeson
+import           Data.Aeson                ()
 import           Data.ByteString           (ByteString, length)
 import           Data.ByteString.Lazy      (fromStrict)
 import           Data.Kind                 ()
-import           Data.Text                 (Text, pack)
-import           Network.HTTP.Types        (status200)
+import           Flow                      ((<|))
+import           Network.HTTP.Types
+    ( methodDelete
+    , methodGet
+    , methodPost
+    , methodPut
+    , status200
+    )
 import           Network.HTTP.Types.Header (hContentType)
 import           Network.HTTP.Types.URI    (Query)
 import           Network.Wai
-    ( Application
+    ( Request
     , Response
+    , ResponseReceived
     , pathInfo
     , queryString
+    , rawPathInfo
+    , requestMethod
+    , responseFile
     , responseLBS
     )
 import           Network.Wai.Handler.Warp  (run)
+
 
 -- | Main Function
 --
 --
 someFunc :: IO ()
 someFunc = do
-    let port = 3000
-    putStrLn $ "listening on " ++ show port
-    run port app
+  putStrLn <| "listening on " <> show port
+  run port app
+  where
+    port :: Int
+    port = 3000
+
 
 -- | Application
 --
 --
-app :: Application
-app req res = do
-    case (pathInfo req, queryString req) of
-      (["expr"], [("q", Just stuff)]) -> res $ homeRoute stuff
-      _                               -> res $ notFoundRoute
+app :: Request                           -- ^ request
+    -> (Response -> IO ResponseReceived) -- ^ handler response to IO
+    -> IO ResponseReceived               -- ^ response
+app request respond
+  | requestMethod request == methodPost   = respond <| post
+  | requestMethod request == methodPut    = respond <| put
+  | requestMethod request == methodDelete = respond <| delete
+  | requestMethod request == methodGet  =
+    case (reqPathInfo, reqQueryString) of
+      ([], _)                         -> respond <| index
+      (["expr"], [("q", Just stuff)]) -> respond <| homeRoute stuff
+      _                               -> respond <| notFoundRoute
+    where
+      reqPathInfo = pathInfo request
+      reqQueryString = queryString request
+
+
+index :: Response
+index =
+  responseFile status200 [(hContentType, "text/html")] "www/index.html" Nothing
 
 -- | JSON Response
 --
 --
 homeRoute :: ByteString -> Response
 homeRoute bs =
-    responseLBS
-        status200
-        [(hContentType, "application/json")]
-        (fromStrict bs)
+  responseLBS status200 [(hContentType, "application/json")] (fromStrict bs)
+
 
 -- | Page not found
 --
 --
 notFoundRoute :: Response
 notFoundRoute =
-    responseLBS
-        status200
-        [(hContentType, "text/plain")]
-        "Page not found."
+  responseLBS status200 [(hContentType, "text/plain")] "Page not found."
 
--- | JSON data
+-- | Post
 --
 --
-jsonData :: [Text]
-jsonData = (pack . show) <$> [1 .. 100]
+post :: Response
+post =
+  responseLBS status200 [(hContentType, "text/plain")] "POST method"
 
+-- | PUT
+--
+--
+put :: Response
+put =
+  responseLBS status200 [(hContentType, "text/plain")] "PUT method"
+
+-- | Delete
+--
+--
+delete :: Response
+delete =
+  responseLBS status200 [(hContentType, "text/plain")] "DELETE method"
